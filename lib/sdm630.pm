7e853c56409927a08d821d81be8ea107d14d917f
berni <berni@shellserver.berni.lan>
2017-10-01 06:40:42 +0200

    lib: retrieve all vaues

--- lib/sdm630.pm
+++ lib/sdm630.pm
@@ -15,6 +15,14 @@ $Data::Dumper::Indent = 1;
 use Device::Modbus::TCP::Client;
 
 
+sub retrieve_all {
+  my $ref_client = shift;
+  my $ref_values = {};
+  SDM630::retrieve($ref_client, 0, 3, ['Voltage', 'Current', 'Power_W', 'Power_VA', 'Power_Var', 'PowerFactor', 'phi'], $ref_values);
+  SDM630::retrieve($ref_client, 21, 1, ['Voltage_avg', '_23', 'Current_avg', 'Current_sum', '_26', 'Power_W_sum', '_28', 'Power_VA_sum', '_30', 'Power_Var_sum', 'PowerFactor_sum', '_33', 'phi_sum', '_35', 'Frequency_Hz', 'Energy_kWh_in', 'Energy_kWh_out', 'Energy_kVarh_in', 'Energy_kVarh_out', 'Energy_kVAh', 'Charge_Ah', 'Power_W_demand_tot', 'Power_W_demand_max', ], $ref_values);
+  return $ref_values;
+}
+
 
 sub retrieve {
   my $ref_client = shift;
@@ -95,5 +103,32 @@ sub output_values {
   }
   printf("%-20s %s\n", $path, $string);
 }
+
+
+sub create_all_rrds {
+  my @params = (
+    { 'name' => 'charge',         'type' => 'COUNTER', 'rows' => ['Ah:0:U', ], },
+    { 'name' => 'current',        'type' => 'GAUGE',   'rows' => ['L1:0:100', 'L2:0:100', 'L3:0:100', 'avg:0:100', 'sum:0:100', ], },
+    { 'name' => 'energy_kvah',    'type' => 'GAUGE',   'rows' => ['kVAh:0:U', ], },
+    { 'name' => 'energy_kvarh',   'type' => 'GAUGE',   'rows' => ['in:0:U', 'out:0:U', ], },
+    { 'name' => 'energy_kwh',     'type' => 'GAUGE',   'rows' => ['in:0:U', 'out:0:U', ], },
+    { 'name' => 'frequency',      'type' => 'GAUGE',   'rows' => ['Hz:45:55', ], },
+    { 'name' => 'phi',            'type' => 'GAUGE',   'rows' => ['L1:-180:180', 'L2:-180:180', 'L3:-180:180', 'sum:-180:180', ], },
+    { 'name' => 'power_va',       'type' => 'GAUGE',   'rows' => ['L1:0:20000', 'L2:0:20000', 'L3:0:20000', 'sum:0:60000', ], },
+    { 'name' => 'power_var',      'type' => 'GAUGE',   'rows' => ['L1:0:20000', 'L2:0:20000', 'L3:0:20000', 'sum:0:60000', ], },
+    { 'name' => 'power_w',        'type' => 'GAUGE',   'rows' => ['L1:0:20000', 'L2:0:20000', 'L3:0:20000', 'sum:0:60000', ], },
+    { 'name' => 'power_w_demand', 'type' => 'GAUGE',   'rows' => ['max:0:60000', 'tot:0:60000', ], },
+    { 'name' => 'powerfactor',    'type' => 'GAUGE',   'rows' => ['L1:-1:1', 'L2:-1:1', 'L3:-1:1', 'sum:-1:1', ], },
+    { 'name' => 'voltage',        'type' => 'GAUGE',   'rows' => ['L1:0:270', 'L2:0:270', 'L3:0:270', 'avg:0:270', ], },
+  );
+
+  foreach my $ref_param (@params)
+  {
+    $ref_param->{'step'} = 10;
+    $ref_param->{'resolutions'} = ['2H@10S', '2d@1M', '2w@10M', '2m@1H', '2y@12H', ];
+    create_rrd($ref_param);
+  }
+}
+
 
 1;
--- sdm630-read.pl
+++ sdm630-read.pl
@@ -20,14 +20,9 @@ my $count = 21;
 my $start = 0;
  
 my $ref_client = Device::Modbus::TCP::Client->new(host => "192.168.178.16", timeout => 2);
- 
-my $ref_values = {};
- 
-SDM630::retrieve($ref_client, 0, 3, ["Voltage", "Current", "Power_W", "Power_VA", "Power_Var", "PowerFactor", "phi"], $ref_values);
-SDM630::retrieve($ref_client, 21, 1, ["Voltage_avg", "_23", "Current_avg", "Current_sum", "_26", "Power_W_sum", "_28", "Power_VA_sum", "_30", "Power_Var_sum", "PF_sum", "_33", "phi_sum", "_35", "Frequency_Hz", "Energy_kWh_in", "Energy_kWh_out", "Energy_kVarh_in", "Energy_kVarh_out", "Energy_kVAh", "Charge_Ah", "Power_W_demand_tot", "Power_W_demand_max", ], $ref_values);
+my $ref_values = SDM630::retrieve_all($ref_client);
+$ref_client->disconnect;
+
 SDM630::output_values($ref_values);
-
-print Dumper($ref_values);
-exit 0;
-
+#print Dumper($ref_values);
 
