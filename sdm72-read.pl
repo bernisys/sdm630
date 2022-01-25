8ae1c978d04b1006b93b5f370554a2c07c938596
berni <berni@shellserver.berni.lan>
2022-01-25 12:40:58 +0100

    continuous reading

--- sdm72-read.pl
+++ sdm72-read.pl
@@ -18,29 +18,31 @@ use sdm630;
 my $file = shift || 'sdm630.conf';
 my $ref_config = SDM630::read_config($file);
 
-my $ref_client = Device::Modbus::TCP::Client->new(host => $ref_config->{'IP_ADDRESS'}, timeout => $ref_config->{'TIMEOUT'});
 while (1==1) {
+  my $now = qx/date/;
   my %values;
+  my @output = ();
+  my $ref_client = Device::Modbus::TCP::Client->new(host => $ref_config->{'IP_ADDRESS'}, timeout => $ref_config->{'TIMEOUT'});
   foreach my $ref_device (@{$ref_config->{'DEVICE'}}) {
-    printf("%s (%s)\n", $ref_device->{'NAME'}, $ref_device->{'TYPE'});
     my $ref_values = SDM630::retrieve_all($ref_client, $ref_device->{'UNIT'}, $ref_device->{'TYPE'});
-    print SDM630::output_values($ref_values);
     $values{$ref_device->{'UNIT'}} = {
       'type' => $ref_device->{'TYPE'},
       'data' => $ref_values,
     };
+
+    push @output, sprintf("%s (%s)\n", $ref_device->{'NAME'}, $ref_device->{'TYPE'}), SDM630::output_values($ref_values), "\n";
+
     SDM630::feed_rrds($ref_values, $ref_device->{'NAME'});
     if ($ref_device->{'TYPE'} eq 'SDM630') {
       SDM630::feed_rrds($ref_values);
     }
-
-    print "\n";
   }
+  $ref_client->disconnect;
+
   my $sleeptime = 10 - time % 10;
-  print "$sleeptime\n";
+  print join('', $now, "\n", @output, $sleeptime, "\n");
   sleep $sleeptime;
 }
 
-$ref_client->disconnect;
 
 
