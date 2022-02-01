0bf9dabcd50a1ca21054060d8c47050c3ee5bde3
berni <berni@shellserver.berni.lan>
2022-02-01 05:37:55 +0100

    regenerate complete web output

--- graph.pl
+++ graph.pl
@@ -10,22 +10,19 @@ use Data::Dumper;
 $Data::Dumper::Sortkeys = 1;
 $Data::Dumper::Indent = 1;
 
-use RRDs;
-
 use lib "lib";
 use sdm630;
 
-my $OUTPUT='/home/user/berni/public_html/powermeter';
 my $file = shift || 'sdm630.conf';
 my $ref_config = SDM630::read_config($file);
 
+my $now = qx/date/;
+chomp $now;
+
+printf("@%s\n", $now);
+
 foreach my $ref_device (@{$ref_config->{'DEVICE'}}) {
-  if (! -f $OUTPUT.'/'.$ref_device->{'NAME'}.'/index.html') {
-    SDM630::generate_indexes($OUTPUT, $ref_device->{'TYPE'}, $ref_device->{'NAME'});
-  }
-  SDM630::generate_diagrams($OUTPUT, $ref_device->{'TYPE'}, $ref_device->{'NAME'}, @ARGV);
+  SDM630::generate_indexes($ref_config->{'WEBDIR'}, $ref_device->{'TYPE'}, $ref_device->{'NAME'});
+  SDM630::generate_diagrams($ref_config->{'WEBDIR'}, $ref_device->{'TYPE'}, $ref_device->{'NAME'}, 1, @ARGV);
 }
-
-exit 0;
-
 
